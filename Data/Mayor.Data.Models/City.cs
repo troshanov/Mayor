55e397ea89511eeab583be60e4a010e9d4c475a7
Stoyan Troshanov <troshanov994@gmail.com>
2020-11-15 00:17:29 +0200

    TestCommit

--- Data/Mayor.Data.Models/City.cs
+++ Data/Mayor.Data.Models/City.cs
@@ -13,7 +13,7 @@
         }
 
         [Required]
-        [MaxLength(20)]
+        [MaxLength(30)]
         public string Name { get; set; }
 
         public ICollection<Address> Addresses { get; set; }
